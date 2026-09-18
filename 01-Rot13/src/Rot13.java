c895482ecdc6505e162f85bb7649fb80265d9f70
adrianavp26 <2526_adriana.vallespi@iticbcn.cat>
2026-09-18 16:43:02 +0200

    notes eliminades + iniciat ex 2

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -25,44 +25,36 @@ public class Rot13 {
 
     }
 
-    //envia un string y retorna un string 
     public static String xifraRot13(String cadena){
         String xifrat = "";
-        //primero hay que recorrer la frase para ir guardando caracteres
+
         for (int i = 0; i < cadena.length(); i++) {
             char lletra = cadena.charAt(i);
-            //miramos si es mayus o minus
+
             if(Character.isUpperCase(lletra)){
                 int indice = -1;
-                //comparamos letra con el caracter mayus
+
                 for (int j = 0; j < majuscules.length; j++) {
 
                     if(majuscules[j] == lletra){
-                        //guardamos el indice para luego poder sumar 13
                         indice = j;
                     }
                 }
-                //calculamos la nueva posicion y tenemos en cuenta que el numero nunca supere el largo de los mayus
                 int nuevoIndice = (indice + 13) % majuscules.length;
-                // añadimos la nueva letra
+                
                 xifrat += majuscules[nuevoIndice];
             }else if(Character.isLowerCase(lletra)){
-                //lo mismo que con las mayus pero ahora con las minus
                 int indice = -1;
-                //comparamos letra con el caracter mayus
-                for (int j = 0; j < minuscules.length; j++) {
 
+                for (int j = 0; j < minuscules.length; j++) {
                     if(minuscules[j] == lletra){
                         indice = j;
                     }
                 }
-                //calculamos la nueva posicion y tenemos en cuenta que el numero nunca supere el largo de los mayus
-                //por eso usamos el modulo para volver a empezar
                 int nuevoIndice = (indice + 13) % minuscules.length;
-                // añadimos la nueva letra
+                
                 xifrat += minuscules[nuevoIndice];
             }else{
-                //entonces, si no son caracteres no se tienen que cifrar
                 xifrat += lletra;
             }
         }
@@ -72,37 +64,29 @@ public class Rot13 {
 
     public static String desxifraRot13(String cadena){
         String desxifrat = "";
-        //es lo mismo pero restando 13 en vez de sumar 
-        
-        //primero hay que recorrer la frase para ir guardando caracteres
+
         for (int i = 0; i < cadena.length(); i++) {
             char lletra = cadena.charAt(i);
-            //miramos si es mayus o minus
             if(Character.isUpperCase(lletra)){
                 int indice = -1;
-                //comparamos letra con el caracter mayus
                 for (int j = 0; j < majuscules.length; j++) {
 
                     if(majuscules[j] == lletra){
                         indice = j;
                     }
                 }
-                //calculamos la nueva posicion y tenemos en cuenta que el numero nunca supere el largo de los mayus
                 int nuevoIndice = ((indice - 13) + minuscules.length) % minuscules.length;
-                // añadimos la nueva letra
+                
                 desxifrat += majuscules[nuevoIndice];
             }else if(Character.isLowerCase(lletra)){
                 int indice = -1;
-                //comparamos letra con el caracter mayus
-                for (int j = 0; j < minuscules.length; j++) {
 
+                for (int j = 0; j < minuscules.length; j++) {
                     if(minuscules[j] == lletra){
-                        //guardamos el indice para luego poder sumar 13
                         indice = j;
                     }
                 }
                 int nuevoIndice = ((indice - 13) + minuscules.length) % minuscules.length;
-                // añadimos la nueva letra
                 desxifrat += minuscules[nuevoIndice];
             }else{
                 desxifrat += lletra;
