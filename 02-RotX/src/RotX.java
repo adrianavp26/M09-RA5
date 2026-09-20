adf0c736d38fdc2374bcfa9bf4a88a58a4f03b47
adrianavp26 <2526_adriana.vallespi@iticbcn.cat>
2026-09-20 13:34:15 +0200

    acabat exercici2 RotX

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -1,8 +1,8 @@
 public class RotX {
 
-    static String[] lletras = {"aáàbcçdeéèfghiíìïjklmnñoóòpqrstuúùüvwxyz"};
-    static char[] minuscules = ;
-    static char[] majuscules = ;
+    static String lletres = "aáàbcçdeéèfghiíìïjklmnñoóòpqrstuúùüvwxyz";
+    static char[] minuscules = lletres.toCharArray();
+    static char[] majuscules = lletres.toUpperCase().toCharArray();
 
     public static void main(String[] args) {
         String[] palabras = {"ABC", "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és?"};
@@ -22,15 +22,48 @@ public class RotX {
             xifrat[i] = desxifraRotX(xifrat[i], numero[i]);
             System.out.println(xifrat[i]);
         }
+        System.out.println();
         
+        String frase = "Úiüht, úiü wx ùxì ív?";
+        forcaBrutaRotX(frase);
+        
+    }
+
+    //metode per buscar la lletra en el nostre propi alfabet
+    public static int buscarLletra(char lletra, char[] alfabet){
+        for (int i = 0; i < alfabet.length; i++) {
+            if(alfabet[i] == lletra){
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static String xifraRotX(String cadena, int desplaçament){
         String xifrat = "";
 
+        int indice;
+        char[] alfabetUtilitzat;
+
         for (int i = 0; i < cadena.length(); i++) {
             char lletra = cadena.charAt(i);
+            indice =  buscarLletra(lletra, majuscules);
 
+            if(indice == -1){
+                indice = buscarLletra(lletra, minuscules);
+                alfabetUtilitzat = minuscules;
+            }else{
+                alfabetUtilitzat = majuscules;
+            }
+
+            //si no lo ha encontrado todavia no es una letra
+            if(indice == -1){
+                xifrat += lletra;
+            }else{
+                //si lo ha encotrado:
+                int nuevoIndice = (indice + desplaçament) % alfabetUtilitzat.length;
+                xifrat += alfabetUtilitzat[nuevoIndice];
+            }
         }
 
         return xifrat;
@@ -38,13 +71,42 @@ public class RotX {
 
     public static String desxifraRotX(String cadena, int desplaçament){
         String desxifrat = "";
+        int indice;
+        char[] alfabetUtilitzat;
 
+        for (int i = 0; i < cadena.length(); i++) {
+            char lletra = cadena.charAt(i);
+            indice =  buscarLletra(lletra, majuscules);
+
+            if(indice == -1){
+                indice = buscarLletra(lletra, minuscules);
+                alfabetUtilitzat = minuscules;
+            }else{
+                alfabetUtilitzat = majuscules;
+            }
+
+            //si no lo ha encontrado todavia no es una letra
+            if(indice == -1){
+                desxifrat += lletra;
+            }else{
+                //si lo ha encotrado:
+                int nuevoIndice = ((indice - desplaçament) + alfabetUtilitzat.length) % alfabetUtilitzat.length;
+                desxifrat += alfabetUtilitzat[nuevoIndice];
+            }
+        }
 
         return desxifrat;
     }
 
-    public static String forcaBrutaRotX(String cadenaXifrada){
+    public static void  forcaBrutaRotX(String cadenaXifrada){
+        String resultat;
+        System.out.println("Missatge xifrat: " + cadenaXifrada);
 
-        return"";
+        //hay que recorrer el alfabeto entero
+        for (int i = 0; i < majuscules.length; i++) {
+            resultat = desxifraRotX(cadenaXifrada, i);
+            System.out.printf("(%d) -> %s%n", i, resultat);
+        }
+        
     }
 }
